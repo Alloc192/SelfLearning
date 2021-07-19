7f8dfe651f69b0b68408c22f7fc62f7cc831112f
jwC-electr <55658884+jwC-electr@users.noreply.github.com>
2021-07-19 10:55:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
-#include <iostream>
-#include <string>
+// #include "CreationalPatterns/factory_method.h"
+// #include "CreationalPatterns/abstract_factory.h"
+// #include "CreationalPatterns/builder.h"
+// #include "CreationalPatterns/prototype.h"
+// #include "CreationalPatterns/singleton.h"
 
-// #include "factory_method.h"
-// #include "abstract_factory.h"
-#include "builder.h"
+// #include "StructuralPatterns/adapter.h"
+// #include "StructuralPatterns/bridge.h"
+// #include "StructuralPatterns/composite.h"
+// #include "StructuralPatterns/decorator.h"
+// #include "StructuralPatterns/facade.h"
+// #include "StructuralPatterns/flywight.h"
+// #include "StructuralPatterns/proxy.h"
+
+// #include "BehavioralPatterns/chain_of_responsibility.h"
+// #include "BehavioralPatterns/command.h"
+// #include "BehavioralPatterns/iterator.h"
+// #include "BehavioralPatterns/mediator.h"
+// #include "BehavioralPatterns/memento.h"
+// #include "BehavioralPatterns/observer.h"
+// #include "BehavioralPatterns/state.h"
+// #include "BehavioralPatterns/strategy.h"
+// #include "BehavioralPatterns/template_method.h"
+#include "BehavioralPatterns/visitor.h"
 
 int main()
 {
